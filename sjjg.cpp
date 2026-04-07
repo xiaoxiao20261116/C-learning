ed57dad7479f345906d174e4e4ea792e1ac1165d
xiaoxiao20261116 <3227857793@qq.com>
2026-04-07 21:13:10 +0800

    Add files via upload

---
+++ sjjg.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdbool.h>
+#define maxsize 100
+typedef struct
+{
+	int data[maxsize];
+	int length;
+}sqList;
+bool ListInsert(sqList* L, int pos, int val)
+{
+	if (pos<1 || pos>L->length + 1)
+		return false;
+	if ( L->length>= maxsize)
+		return false;
+	for (int i = L->length; i > pos; i--)
+	{
+		L->data[i] = L->data[i - 1];
+	}
+	L->data[pos - 1] = val;
+	L->length++;
+	return true;
+}
+void printList(sqList* L)
+{
+	int i;
+	if (L->length == 0)return;
+	printf("%d", L->data[0]);
+	for (i = 1; i < L->length ; i++)
+	{
+		printf(",%d", L->data[i]);
+	}
+}
+int main()
+{
+	sqList L;
+	L.length = 0;
+	L.data[0] = 7, L.data[1] = 8, L.data[2] = 3;
+	L.length = 3;
+	printList(&L);
+	printf("\n");
+	ListInsert(&L, 1, 9);
+	printList(&L);
+	return 0;
+}
