96cb97908eb73592aaa495fc6e45c796fdfdc234
xiaoxiao20261116 <3227857793@qq.com>
2026-04-08 17:10:15 +0800

    Update sqlist_insert.c

--- sqlist_insert.c
+++ sqlist_insert.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<stdbool.h>
 #define maxsize 100
-typedef struct
+typedef struct   //定义结构体，名称
 {
 	int data[maxsize];
 	int length;
 }sqList;
-bool ListInsert(sqList* L, int pos, int val)
+bool ListInsert(sqList* L, int pos, int val)//判断是否满足条件
 {
-	if (pos<1 || pos>L->length + 1)
+	if (pos<1 || pos>L->length + 1)  //位置在有效长度里
 		return false;
-	if ( L->length>= maxsize)
+	if ( L->length>= maxsize)  //长度不能超过最大
 		return false;
-	for (int i = L->length; i > pos; i--)
+	for (int i = L->length; i > pos; i--)  //遍历插入位置后的所有数，并使它们向后移一个单位
 	{
 		L->data[i] = L->data[i - 1];
 	}
@@ -20,7 +20,7 @@ bool ListInsert(sqList* L, int pos, int val)
 	L->length++;
 	return true;
 }
-void printList(sqList* L)
+void printList(sqList* L)  //用来输出
 {
 	int i;
 	if (L->length == 0)return;
@@ -32,13 +32,13 @@ void printList(sqList* L)
 }
 int main()
 {
-	sqList L;
+	sqList L;  //初始化
 	L.length = 0;
 	L.data[0] = 7, L.data[1] = 8, L.data[2] = 3;
 	L.length = 3;
 	printList(&L);
 	printf("\n");
 	ListInsert(&L, 1, 9);
-	printList(&L);
+	printList(&L);  //判断插入是否成功
 	return 0;
 }
